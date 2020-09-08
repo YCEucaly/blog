2117697c40619917857601cd52fa8d9c4819533c
youzouzou <573257706@qq.com>
2020-09-08 23:40:17 +0800

    Update global.js

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,5 +1,5 @@
 const defaultCover = "https://youzouzou.run/common/assets/1.jpg";
-const httpUrl = "http://localhost:8011";// "https://youzouzou.run:8011";
+const httpUrl = "https://youzouzou.run:8011";
 
 export default {
     defaultCover,
